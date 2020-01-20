4283e52322cec398749c111b2a85e239a6ba63e9
mayur.kanojiya <mayur.kanojiya@hiddenbrains.in>
2020-01-20 18:13:30 +0530

    make it better

--- manage.py
+++ manage.py
@@ -6,7 +6,6 @@ from app import app
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
-    # from django.core.management import execute_from_command_line
-    #
-    # execute_from_command_line(sys.argv)
-    app.run(debug=True)
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
